d9c0ac9438a5c27d03a251139be967f6ec15b0a9
Alejandro <aleig.906@gmail.com>
2025-05-12 11:15:32 -0300

    chore: dev branch github actions

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   apps: [
     {
-      name: "devtacora-backend", // Este es el nombre del proceso en PM2, lo que ves con "pm2 list"
-      script: "./dist/index.js", // Este es el archivo que se ejecuta
+      name: "devtacora-dev",
+      script: "./dist/index.js",
       instances: 1,
       autorestart: true,
       watch: false,
